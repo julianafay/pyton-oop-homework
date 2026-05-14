20f851635df73ebdab9c234ac5d82c84dc1bc0b9
on test <julmilk4@gmail.com>
2026-05-14 18:15:13 +0300

    Первый коммит: классы Mentor, Lecturer, Reviewer

---
+++ main.py
@@ -0,0 +1,25 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
+if __name__ == "__main__":
+    lecturer = Lecturer("Иван", "Иванов")
+    reviewer = Reviewer("Пётр", "Петров")
+
+    print(isinstance(lecturer, Mentor))   # True
+    print(isinstance(reviewer, Mentor))   # True
+    print(lecturer.courses_attached)      # []
+    print(reviewer.courses_attached)      # []
